5beef8a0eada1fb192768c8dcdb46638f383c144
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-11 22:20:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,68 @@
+# provider identity parameters
+variable "api_fingerprint" {
+  description = "fingerprint of oci api key"
+  type        = string
+}
+
+variable "api_private_key_path" {
+  description = "path to oci api private key used"
+  type        = string
+}
+
+variable "region" {
+  description = "the oci region where resources will be created"
+  type        = string
+}
+
+variable "tenancy_id" {
+  description = "tenancy id where to create the sources"
+  type        = string
+}
+
+variable "user_id" {
+  description = "id of user that terraform will use to create the resources"
+  type        = string
+}
+
+# general oci parameters
+
+variable "Parent_compartment_id" {
+  description = "compartment id where to create all resources"
+  type        = string
+}
+
+variable "label_prefix" {
+  description = "a string that will be prepended to all resources"
+  type        = string
+  default     = "FIN"
+}
+
+#Netwroking Variables 
+
+variable "vcn_cidr_blocks" {
+  description = "The list of IPv4 CIDR blocks the VCN will use."
+  default     = ["10.0.0.0/16"]
+  type        = list(string)
+}
+
+variable "vcn_dns_label" {
+  description = "A DNS label for the VCN"
+  type        = string
+}
+
+variable "vcn_name" {
+  type        = string
+  default     = "retail-sydney-vcn"
+}
+
+
+variable "network_compartment" {
+  description = "network compartment"
+  type        = string
+}
+
+
+variable "subnets" {
+  type    = any
+  default = {}
+}
