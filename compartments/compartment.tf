9af8c3561d0545eda41d8460c4ea0d4aa0611c37
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-11 22:48:45 +0530

    Update compartment.tf

--- compartments/compartment.tf
+++ compartments/compartment.tf
@@ -1,10 +1,18 @@
-module "compartment" {
+module "base_compartment" {
   source              = "./compartment"
   tenancy_ocid            = var.tenancy_ocid
-  compartment_id          = var.tenancy_ocid
-  compartment_name        = "tf_example_compartment"
+  compartment_id          = var.Parent_compartment_id
+  compartment_name        = "Retail"
   compartment_description = "compartment created by terraform"
   compartment_create      = true
   enable_delete           = true
-  count = length(var.subnets) > 0 ? 1 : 0
+}
+
+  module "base_sub_compartments" {
+  source              = "./compartment"
+  compartments        = var.sub_compartments
+  compartment_id      = module.base_compartment.compartment_id_output
+  compartment_create      = true
+  enable_delete           = true
+  count = length(var.sub_compartments) > 0 ? 1 : 0
 }
