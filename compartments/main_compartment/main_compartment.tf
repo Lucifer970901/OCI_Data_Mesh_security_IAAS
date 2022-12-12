3a1954b4f0e13ea1e81487396ed58a4e3fbc36f3
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-12 17:12:02 +0530

    Update main_compartment.tf

--- compartments/main_compartment/main_compartment.tf
+++ compartments/main_compartment/main_compartment.tf
@@ -1,5 +1,5 @@
 #VCN resource block
-resource "oci_identity_compartment" "Retail_compartment" {
+resource "oci_identity_compartment" "main_compartment" {
     compartment_id = var.Parent_compartment_id
     display_name = var.maincompartment_name
     compartment_create      = true # if false, a data source with a matching name is created instead
