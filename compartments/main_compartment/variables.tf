6955f22a937daffc87a200dbcc262251f7a8ab2f
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-12 16:00:32 +0530

    Create variables.tf

---
+++ compartments/main_compartment/variables.tf
@@ -0,0 +1,5 @@
+variable "maincompartment_name" {
+  description = "user-friendly name of to use for the compartment to be appended to the label_prefix"
+  type        = string
+  default     = "Retail"
+}
