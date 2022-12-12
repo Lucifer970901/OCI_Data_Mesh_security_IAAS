636ea5a56b30d5098b53cf4466b26a24069b9f60
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-12 15:21:52 +0530

    Update compartment.tf

--- compartments/compartment/compartment.tf
+++ compartments/compartment/compartment.tf
@@ -2,7 +2,7 @@ resource "oci_identity_compartment" "sub_compartments" {
     #Required
      
     for_each                = var.compartments
-    compartment_id          = each.value.cidr_block    
+    compartment_id          = each.value.compartment_id  
     display_name            = lookup(each.value, "name", each.key)
     description             = each.value.description
     compartment_create      = each.value.compartment_create 
